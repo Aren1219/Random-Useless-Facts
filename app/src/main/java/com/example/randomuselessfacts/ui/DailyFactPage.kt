5110ab7ddf486399da32ef58fb7f94bb4f13f4fd
Aren1219 <arenwang1219@gmail>
2022-09-16 09:44:07 +0100

    improved main page save icons. changed app name to "Random Facts"

--- app/src/main/java/com/example/randomuselessfacts/ui/DailyFactPage.kt
+++ app/src/main/java/com/example/randomuselessfacts/ui/DailyFactPage.kt
@@ -1,7 +1,6 @@
 package com.example.randomuselessfacts.ui
 
 import android.widget.Toast
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.ClickableText
@@ -9,6 +8,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -27,13 +27,16 @@ import com.example.randomuselessfacts.ui.theme.RandomUselessFactsTheme
 import com.example.randomuselessfacts.util.Resource
 
 @Composable
-fun DailyFactPage(viewModel: MainViewModel){
+fun DailyFactPage(viewModel: MainViewModel) {
     var showRandom by rememberSaveable { mutableStateOf(false) }
 
     val dailyFact = viewModel.dailyFact.observeAsState()
     val randomFact = viewModel.randomFact.observeAsState()
 
     val context = LocalContext.current
+
+    var saved1 by rememberSaveable { mutableStateOf(false) }
+    var saved2 by rememberSaveable { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
@@ -44,39 +47,56 @@ fun DailyFactPage(viewModel: MainViewModel){
     ) {
         UiState(
             liveData = dailyFact,
-            "Daily useless fact"
-        ) { fact ->
-            viewModel.saveFact(fact)
-            Toast.makeText(context, "Saved!", Toast.LENGTH_SHORT).show()
-        }
+            cardTitle = "Daily Fact",
+            { fact ->
+                viewModel.saveFact(fact)
+                android.widget.Toast.makeText(context, "Saved!", android.widget.Toast.LENGTH_SHORT)
+                    .show()
+                saved1 = true
+            },
+            saved1
+        )
         RandomButton {
             showRandom = true
             viewModel.getRandomFact()
+            saved2 = false
         }
         if (showRandom)
             UiState(
                 liveData = randomFact,
-                "Random useless fact"
-            ) { fact ->
-                viewModel.saveFact(fact)
-                Toast.makeText(context, "Saved!", Toast.LENGTH_SHORT).show()
-            }
+                cardTitle = "Random Fact",
+                { fact ->
+                    viewModel.saveFact(fact)
+                    android.widget.Toast.makeText(
+                        context,
+                        "Saved!",
+                        android.widget.Toast.LENGTH_SHORT
+                    ).show()
+                    saved2 = true
+                },
+                saved2
+            )
     }
 }
 
 @Composable
 fun UiState(
     liveData: State<Resource<Fact>?>,
-    cardTitle:String,
-    saveFact: (Fact) -> Unit
+    cardTitle: String,
+    saveFact: (Fact) -> Unit,
+    saved: Boolean
 ) {
+
     when (liveData.value) {
         is Resource.Success<*> -> {
             FactCard(
                 liveData.value!!.data!!,
                 cardTitle,
-                Icons.Default.Favorite
-            ) { saveFact(liveData.value!!.data!!) }
+                if (saved) Icons.Default.Favorite
+                else Icons.Default.FavoriteBorder
+            ) {
+                saveFact(liveData.value!!.data!!)
+            }
         }
         is Resource.Loading -> {
             CircularProgressIndicator()
@@ -128,19 +148,27 @@ fun FactCard(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(30.dp),
-            ){
+            ) {
                 ClickableText(
                     modifier = Modifier.align(Alignment.TopStart),
                     text = annotatedString,
                     onClick = { offset ->
-                        annotatedString.getStringAnnotations(tag = "url", start = offset, end = offset)
+                        annotatedString.getStringAnnotations(
+                            tag = "url",
+                            start = offset,
+                            end = offset
+                        )
                             .firstOrNull()?.let {
-                                uriHandler.openUri(it.item)
+                                try {
+                                    uriHandler.openUri(it.item)
+                                } catch (e: Exception) {
+                                    e.printStackTrace()
+                                }
                             }
                     }
                 )
                 IconButton(
-                    onClick = {onClick()},
+                    onClick = { onClick() },
                     modifier = Modifier.align(Alignment.TopEnd)
                 ) {
                     Icon(imageVector = buttonIcon, null)
@@ -151,22 +179,22 @@ fun FactCard(
 }
 
 @Composable
-fun RandomButton(onClick:() -> Unit){
-    Button(onClick = {onClick()}) {
+fun RandomButton(onClick: () -> Unit) {
+    Button(onClick = { onClick() }) {
         Text(text = "Get random fact")
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewDailyFact(){
+fun PreviewDailyFact() {
     RandomUselessFactsTheme {
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            FactCard(fact, "Daily useless fact",Icons.Default.Favorite) {}
+            FactCard(fact, "Daily useless fact", Icons.Default.FavoriteBorder) {}
             RandomButton {}
         }
     }
